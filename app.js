d52767619ba13851cd35786731c268ef34d15717
Shawn Price <shawnp@gmail.com>
2011-06-21 17:50:25 -0700

    Covers Episode 10 of nodetuts.com. Tweaked to use new Express conventions.

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
 var express = require('express');
+var products = require('./products');
 
 var app = express.createServer();
 
-app.configure('development', function () {
+// Configuration
+
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
   app.use(express.logger());
-  app.use(express.errorHandler({
-    dumpExceptions: true,
-    showStack: true
-  }));
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function () {
-  app.use(express.logger());
   app.use(express.errorHandler());
 });
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+// Routes
 
 app.get('/', function(req, res) {
   res.render('root');
 });
 
-app.listen(4000);
+app.get('/products', function(req, res) {
+  res.render('products/index', {
+    products: products.all
+  });
+});
+
+app.get('/products/:id', function(req, res) {
+  var product = products.find(req.params.id);
+  res.render('products/show', {
+    product: product
+  });
+});
+
+// Only listen on $ node app.js
+
+if (!module.parent) {
+  app.listen(4000);
+  console.log("Express server listening on port %d", app.address().port);
+}
